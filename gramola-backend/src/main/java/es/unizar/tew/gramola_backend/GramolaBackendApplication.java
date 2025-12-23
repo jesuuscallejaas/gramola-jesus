ace320a558a9c1ad38275b064e5e0eb13cbe59d9
Jesús Callejas Soto <jesuscs2004@gmail.com>
2025-12-23 20:50:50 +0100

    Creacion de fromt y backend

---
+++ gramola-backend/src/main/java/es/unizar/tew/gramola_backend/GramolaBackendApplication.java
@@ -0,0 +1,13 @@
+package es.unizar.tew.gramola_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GramolaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GramolaBackendApplication.class, args);
+	}
+
+}
---
+++ gramola-backend/src/test/java/es/unizar/tew/gramola_backend/GramolaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package es.unizar.tew.gramola_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GramolaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
